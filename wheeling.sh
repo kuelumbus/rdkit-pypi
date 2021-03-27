e68615c386d04cdda58bf0233b8bbcf979869325
Chris <christopher.kuenneth@gmail.com>
2021-03-27 16:19:24 -0400

    Fix typos

--- wheeling.sh
+++ wheeling.sh
@@ -10,46 +10,45 @@ function repair_wheel {
     fi
 }
 
-# Downbload boost
+# Download boost 
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 
-# Fix boost's detection issue of the python include dir 
+# Fix: Boost has an issue detecting the include directory of python3.7m (because of the "m")
 ln -fs  /opt/python/cp37-cp37m/include/python3.7m /opt/python/cp37-cp37m/include/python3.7
 
-# Specify python version 
-
+# Python versions
 versions=( /opt/python/cp37-cp37m/bin /opt/python/cp38-cp38/bin)
 for PYBIN in "${versions[@]}"; do
     export PYBIN
     export BOOST_ROOT="${PYBIN}/../boost/"
 
-    # build boost 
+    # Build boost
     tar -xzf boost_1_67_0.tar.gz
     cd boost_1_67_0 
     ./bootstrap.sh --with-libraries=python,serialization,iostreams,system,regex --with-python="${PYBIN}/python" --with-python-root="${PYBIN}/../"
     ./b2 install --prefix="${BOOST_ROOT}" -j 20
     cd .. 
     
-    # install pip packages
+    # Install packages necessary for the build
     "${PYBIN}/pip" install cmake-build-extension numpy
 
-    # used in the setup.py
+    # Used later in setup.py
     export PYINC=`echo  ${PYBIN}/../include/python*/ | xargs -n 1 | tail -n 1`
     export LD_LIBRARY_PATH=/rdkit/lib:$BOOST_ROOT/lib
 
-    # copy setup.py for /io
+    # Copy setup.py from /io
     cp /io/setup.py .
 
-    # build wheel
+    # Build wheel
     "${PYBIN}/python" setup.py bdist_wheel -d /io/wheelhouse_pre/
 
-    # We need to run 'bdist_wheel' a second time to include all libs that were build (I am no sure why)
+    # We need to run 'bdist_wheel' a second time to include all libs that were built (I am not sure why)
     "${PYBIN}/python" setup.py bdist_wheel -d /io/wheelhouse_pre/
 
-    # find and include missing libs
+    # Find and include missing libs
     repair_wheel /io/wheelhouse_pre/*.whl
 
-    # clean up
+    # Clean up
     rm -r /io/wheelhouse_pre/
     rm -r boost_1_67_0
 done
