7b61033d99af612b354072aca8ef87426d8b2072
Chris <christopher.kuenneth@gmail.com>
2021-05-18 16:16:39 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ class BuildRDKit(build_ext_orig):
 
 setup(
     name="rdkit-pypi",
-    version=f"2021.03.01",
+    version=f"2021.3.1.5",
     description="A collection of chemoinformatics and machine-learning software written in C++ and Python",
     url="https://github.com/kuelumbus/rdkit_platform_wheels",
     project_urls={
