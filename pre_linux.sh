711278527a250668fa98c813f5827ae8b6c8e5ba
Chris <christopher.kuenneth@gmail.com>
2021-05-25 12:25:42 -0400

    Use centos repo cairo

--- pre_linux.sh
+++ pre_linux.sh
@@ -1,10 +1,10 @@
-yum install -y wget freetype-devel zlib-devel libpng12-devel pixman-devel eigen3-devel
+yum install -y wget freetype-devel zlib-devel libpng12-devel pixman-devel eigen3-devel cairo-devel
 
 # compile cairo (RDKit needs older version than in centOS7 repo)
-wget https://www.cairographics.org/releases/cairo-1.10.0.tar.gz --no-check-certificate
-tar xvf cairo-*
-cd cairo-*
-./configure
-make -j 20
-make install
-cd ..
+# wget https://www.cairographics.org/releases/cairo-1.10.0.tar.gz --no-check-certificate
+# tar xvf cairo-*
+# cd cairo-*
+# ./configure
+# make -j 20
+# make install
+# cd ..
