3c2b64a55125412f476faa7b2f5a36c10f71fdf3
Chris <christopher.kuenneth@gmail.com>
2021-09-11 13:03:25 -0400

    Support building windows wheels (x64):
    * Use delvewheels to copy dlls to the wheels
    * Clean up

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ import os
 from subprocess import check_call, call, run, PIPE
 import sys
 from sys import platform
+import shutil
 
 from pathlib import Path
+
+# get vcpkg path on Github
+vcpkg_path = Path('C:/vcpkg')
+
+
+def towin(pt: Path):
+    """Returns a windows path from a Path object"""
+    return str(pt).replace('\\', '/')
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -21,39 +30,42 @@ class RDKit(Extension):
 
 
 class BuildRDKit(build_ext_orig):
-
     def run(self):
         for ext in self.extensions:
-            # Build boost
+            # Build boot
             self.build_boost(ext)
             # Then RDKit
             self.build_rdkit(ext)
             # Copy files so that a wheels package can be created
             self.create_package(ext)
-            
         # invoke the creation of the wheels package
         super().run()
+
 
     def get_ext_filename(self, ext_name):
         ext_path = ext_name.split('.')
         return os.path.join(*ext_path)
-                
+
     def create_package(self, ext):
         from distutils.file_util import copy_file
         from shutil import copytree, rmtree
 
-        # copy RDKit package
-        rdkit_root = Path(self.build_temp).absolute() / 'rdkit_install/' / 'lib'
-        rdkit_pyfiles = list(rdkit_root.glob('python*'))[0] / 'site-packages' / 'rdkit' 
+        # copy RDKit
+        if platform == 'win32':
+            rdkit_root = Path(self.build_temp).absolute() / 'rdkit_install/' / 'Lib'
+            rdkit_pyfiles = rdkit_root / 'site-packages' / 'rdkit' 
+        else:
+            rdkit_root = Path(self.build_temp).absolute() / 'rdkit_install/' / 'lib'
+            rdkit_pyfiles = list(rdkit_root.glob('python*'))[0] / 'site-packages' / 'rdkit' 
 
         # rdkit needs some files from the Data directory to run correctly 
-        rdkit_data_path = Path(self.build_temp).absolute() / 'rdkit' / 'rdkit' / 'Data'
+        rdkit_data_path = Path(self.build_temp).absolute() / 'rdkit' / 'Data'
 
         # replace line with _share=... with _share = os.path.dirname(__file__) in RDPaths.py
         rdpaths = rdkit_pyfiles / 'RDPaths.py'
 
         # For linux
-        if platform == "linux" or platform == "linux2":
+        if platform == "linux" or platform == "linux2" or platform == 'win32':
             # linux
             call(["sed", "-i", "/_share =/c\_share = os.path.dirname(__file__)", str(rdpaths)])
         elif platform == "darwin":
@@ -78,13 +90,22 @@ class BuildRDKit(build_ext_orig):
         libs_rdkit = list(libs_rdkit_linux) + list(libs_rdkit_macos)
 
         libs_boost = Path(self.build_temp).absolute() / 'boost_install' / 'lib'
-
         libs_boost_linux = libs_boost.glob('*.so*')
         libs_boost_mac = libs_boost.glob('*dylib')
-        libs_boost = list(libs_boost_linux) + list(libs_boost_mac)
+        libs_boost_tmp = list(libs_boost_linux) + list(libs_boost_mac)
 
-        [copy_file(i, '/usr/local/lib' ) for i in libs_rdkit]
-        [copy_file(i, '/usr/local/lib' ) for i in libs_boost]
+        if platform != 'win32':
+            [copy_file(i, '/usr/local/lib' ) for i in libs_rdkit]
+            [copy_file(i, '/usr/local/lib' ) for i in libs_boost_tmp]
+        else:
+            libs_rdkit_win = Path(rdkit_root).glob('*.dll')
+            libs_boost_win = libs_boost.glob('*.dll')
+            
+            libs_vcpkg = list((vcpkg_path / 'installed' / 'x64-windows' / 'bin').glob('*.dll'))
+
+            [copy_file(i, 'C://libs' ) for i in libs_rdkit_win]
+            [copy_file(i, 'C://libs' ) for i in libs_boost_win]
+            [copy_file(i, 'C://libs' ) for i in libs_vcpkg]
     
     def build_boost(self, ext):
         """Build the Boost libraries"""
@@ -98,9 +119,12 @@ class BuildRDKit(build_ext_orig):
 
         # Download and unpack Boost
         os.chdir(str(boost_build_path))
+
         cmds = [
-            f'wget {ext.boost_download_url} --no-check-certificate',
-            f'tar -xzf {Path(ext.boost_download_url).name}',]
+            f'wget {ext.boost_download_url} --no-check-certificate -q',
+            f'tar -xzf {Path(ext.boost_download_url).name}',
+            ]
+
         [check_call(c.split()) for c in cmds]
 
         # Compile Boost
@@ -117,12 +141,51 @@ class BuildRDKit(build_ext_orig):
             ]
         # Ok to fail
         [call(c.split()) for c in cmds]
+         
+   
+        # Change commands for windows
+        if sys.platform == 'win32':
 
-        cmds = [
+            cmds = [
+                f'bootstrap.bat',
+            ]
+            [check_call(c.split()) for c in cmds]
+
+            # Compile for many python versions at the same time?
+            python_inc = Path(get_paths()["include"])
+            python_libs = Path(get_paths()["data"]) / 'libs'
+            python_exe = Path(get_paths()["data"]) / 'python.exe'
+            
+            zlib_include = vcpkg_path / 'packages/zlib_x64-windows/include'
+            zlib_lib = vcpkg_path / 'packages/zlib_x64-windows/lib'
+
+            bzip2_include = vcpkg_path / 'packages/bzip2_x64-windows/include'
+            bzip2_lib = vcpkg_path / 'packages/bzip2_x64-windows/lib'
+            with open('project-config.jam', 'a') as fl:
+                print(f'using python : {sys.version_info[0]}.{sys.version_info[1]} : {towin(python_exe)} : {towin(python_inc)} : {towin(python_libs)} ;', file=fl)
+                print(f' ', file=fl)
+                print(f'using zlib : 2 : <include>{towin(zlib_include)} <search>{towin(zlib_lib)} ;', file=fl)
+                print(f' ', file=fl)
+                print(f'using bzip2 : 1 : <include>{towin(bzip2_include)} <search>{towin(bzip2_lib)} ;', file=fl)
+                print(f' ', file=fl)
+            
+            cmds = [                
+                f'./b2 address-model=64 architecture=x86 link=static link=shared threading=single threading=multi ' \
+                f'variant=release -d0 --abbreviate-paths ' \
+                f'--with-python --with-serialization --with-iostreams --with-system --with-regex --with-program_options ' \
+                f'--prefix={boost_install_path} -j 20 install',
+            ]
+            [check_call(c.split()) for c in cmds]
+
+        else:
+            cmds = [
             f'./bootstrap.sh --with-libraries=python,serialization,iostreams,system,regex --with-python={sys.executable} --with-python-root={Path(sys.executable).parent}/..',
             f'./b2 install --prefix={boost_install_path} -j 20',
-        ]
-        [check_call(c.split()) for c in cmds]
+            ]
+            [check_call(c.split()) for c in cmds]
+
+            
+        check_call(['ls', towin(boost_install_path / 'lib')])
 
         os.chdir(str(cwd))
 
@@ -130,7 +193,7 @@ class BuildRDKit(build_ext_orig):
         """ Build RDKit """
 
         cwd = Path().absolute()
-        rdkit_build_path = Path(self.build_temp).absolute() / 'rdkit'
+        rdkit_build_path = Path(self.build_temp).absolute() 
         rdkit_build_path.mkdir(parents=True, exist_ok=True) 
 
         boost_install_path = Path(self.build_temp).absolute() / 'boost_install'
@@ -146,32 +209,68 @@ class BuildRDKit(build_ext_orig):
         [check_call(c.split()) for c in cmds]
         
         os.chdir(str('rdkit'))
+        
+        # all includes are here
+        vcpkg_install_path = vcpkg_path / 'installed' / 'x64-windows' 
+        vcpkg_include_path = vcpkg_path / 'installed' / 'x64-windows' / 'include'
+        vcpkg_lib_path = vcpkg_path / 'installed' / 'x64-windows' / 'lib'
 
         # Invoke cmake and compile RDKit
-        options = [
-                    f'-DPYTHON_EXECUTABLE={sys.executable}',
-                    f'-DPYTHON_INCLUDE_DIR={get_paths()["include"]}',
+        options = [ 
+            # Defines the paths to many include and libaray paths for windows
+            # Does not work for some reason??
+            # f"-DCMAKE_TOOLCHAIN_FILE=C:\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake" if sys.platform == 'win32' else "",
+            # f"-DVCPKG_TARGET_TRIPLET=x64-windows-static" if sys.platform == 'win32' else "",
 
-                    # RDKIT build flags
-                    f"-DRDK_BUILD_INCHI_SUPPORT=ON",
-                    f"-DRDK_BUILD_AVALON_SUPPORT=ON",
-                    f"-DRDK_BUILD_PYTHON_WRAPPERS=ON",
-                    f"-DRDK_INSTALL_INTREE=OFF",
-                    f"-DRDK_BUILD_CAIRO_SUPPORT=ON",
+            f'-DPYTHON_EXECUTABLE={sys.executable}',
+            f'-DPYTHON_INCLUDE_DIR={get_paths()["include"]}',
 
-                    f"-DBOOST_ROOT={boost_install_path}",
-                    f"-DBoost_NO_SYSTEM_PATHS=ON",
+            # RDKIT build flags
+            f"-DRDK_BUILD_INCHI_SUPPORT=ON",
+            f"-DRDK_BUILD_AVALON_SUPPORT=ON",
+            f"-DRDK_BUILD_PYTHON_WRAPPERS=ON",
+            f"-DRDK_INSTALL_INTREE=OFF",
 
-                    f"-DCMAKE_INSTALL_PREFIX={rdkit_install_path}",
-                    f"-DCMAKE_C_FLAGS=-Wno-implicit-function-declaration",
-                    f"-DCMAKE_CXX_FLAGS=-Wno-implicit-function-declaration",
-        
-                ]
+            # Boost              
+            f"-DBOOST_ROOT={boost_install_path}",
+            f"-DBoost_NO_SYSTEM_PATHS=ON",
+            f"-DBoost_DEBUG=OFF",        
+
+            # Does not work (this is fixed in future rdkit versions I believe)
+            f"-DRDK_INSTALL_STATIC_LIBS=OFF" if sys.platform == 'win32' else "",
+
+            # ##### for windows 
+            # cairo
+            f"-DRDK_BUILD_CAIRO_SUPPORT=ON",
+            f"-DCAIRO_INCLUDE_DIR={towin(vcpkg_include_path)}" if sys.platform == 'win32' else "",
+            f"-DCAIRO_LIBRARY_DIR={towin(vcpkg_lib_path)}" if sys.platform == 'win32' else "",
+            
+            # zlib
+            f"-DZLIB_ROOT={towin(vcpkg_install_path)}" if sys.platform == 'win32' else "",
+
+            # freetype
+            f"-DFREETYPE_INCLUDE_DIRS={towin(vcpkg_include_path)}" if sys.platform == 'win32' else "",
+            f"-DFREETYPE_LIBRARY={towin(vcpkg_lib_path / 'freetype.lib')}" if sys.platform == 'win32' else "",
+
+            # eigen3
+            f"-DEIGEN3_INCLUDE_DIR={towin(vcpkg_include_path)}" if sys.platform == 'win32' else "",
+
+            # instruct to build x64
+            "-Ax64" if sys.platform == 'win32' else "",
+
+            # Mac needs these flags to compile 
+            f"-DCMAKE_C_FLAGS=-Wno-implicit-function-declaration" if sys.platform == 'darwin' else "", 
+            f"-DCMAKE_CXX_FLAGS=-Wno-implicit-function-declaration" if sys.platform == 'darwin' else "", 
+
+            # build stuff
+            f"-DCMAKE_INSTALL_PREFIX={rdkit_install_path}",
+            f"-DCMAKE_BUILD_TYPE=Release",
+        ]
         
         cmds = [
             f"cmake -S . -B build {' '.join(options)} ",
-            f"cmake --build build --verbose -j 5 --config Release",
-            f"make -C build install -j 5"
+            f"cmake --build build --verbose -j 10 --config Release",
+            f"cmake --install build"
         ]    
         [check_call(c.split()) for c in cmds]
         os.chdir(str(cwd))
@@ -180,7 +279,7 @@ class BuildRDKit(build_ext_orig):
 
 setup(
     name="rdkit-pypi",
-    version=f"2021.3.5",
+    version=f"2021.3.5.1",
     description="A collection of chemoinformatics and machine-learning software written in C++ and Python",
     author='Christopher Kuenneth',
     author_email='chris@kuenneth.dev',
@@ -199,7 +298,8 @@ setup(
     ext_modules=[
         RDKit(
             'rdkit',
-            boost_download_url='https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz',
+            # 1.73 does now compile on win for some reason
+            boost_download_url='https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz' if sys.platform != 'win32' else 'https://boostorg.jfrog.io/artifactory/main/release/1.67.0/source/boost_1_67_0.tar.gz',
             rdkit_tag='Release_2021_03_5'
             ),        
     ],
