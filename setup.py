2fcb32fb8bc20eecda309a2ffc94d29d3c12db1e
Chris <christopher.kuenneth@gmail.com>
2022-02-02 09:56:47 -0500

    Install dlls too

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ class BuildRDKit(build_ext_orig):
             options += [
                 "-Ax64",
                 "-DRDK_INSTALL_STATIC_LIBS=OFF",
-                "-DBUILD_SHARED_LIBS=ON",
+                "-DRDK_INSTALL_DLLS_MSVC=ON",
             ]
 
             def to_win_path(pt: Path):
