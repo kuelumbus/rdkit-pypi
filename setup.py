82360edcc9de6f5f8f933b59decbab2be4d08155
Christopher Kuenneth <christopher.kuenneth@gmail.com>
2022-06-27 12:25:54 +0200

    Use string

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext as build_ext_orig
 
 # Set conan logging level
-os.environ['CONAN_LOGGING_LEVEL'] = 10
+os.environ['CONAN_LOGGING_LEVEL'] = '10'
 
 # RDKit version to build (tag from github repository)
 rdkit_tag = "Release_2022_03_3"
