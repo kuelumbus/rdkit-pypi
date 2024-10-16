b857d6f35518bac7a27e091c037551752a5ab487
Christopher Kuenneth <christopher.kuenneth@gmail.com>
2024-10-16 22:24:45 +0200

    oops

--- setup.py
+++ setup.py
@@ -202,8 +202,8 @@ freetype/2.13.2
             f"-DBoost_LIB_VERSION={boost_lib_version}",
             # Select correct python 3 version
             f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
-            f'-DPython3_INCLUDE_DIR={sysconfig.get_path('include')}',
-            f'-DPython3_LIBRARY={sysconfig.get_config_var('LIBDIR')}',
+            f"-DPython3_INCLUDE_DIR={sysconfig.get_path('include')}",
+            f"-DPython3_LIBRARY={sysconfig.get_config_var('LIBDIR')}",
 
             # Point to the system install path of python to correctly find the python.lib file 
             # f"-DPython3_LIBRARY={sysconfig.get_config_var('LIBDIR')}",
